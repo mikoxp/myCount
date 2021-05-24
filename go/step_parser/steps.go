d20bdafe177e971cab6daad548acd50aab5bed86
olesmichalxp@tlen.pl <olesmichalxp@tlen.pl>
2021-05-24 21:00:11 +0200

    step_parser_init

---
+++ go/step_parser/steps.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
+
+type CsvLine struct {
+	Column1 string
+	Column2 string
+}
+
+func main() {
+
+	lines, err := readCsv("Kroki.csv")
+	if err != nil {
+		panic(err)
+	}
+	f, err := os.Create("data.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	for i, line := range lines {
+		if i > 0 {
+			data := CsvLine{
+				Column1: line[0],
+				Column2: line[1],
+			}
+			x := data.Column1 + " " + data.Column2
+			fmt.Println(x)
+			writeToFile(f, x)
+		}
+	}
+}
+
+func readCsv(filename string) ([][]string, error) {
+
+	// Open CSV file
+	f, err := os.Open(filename)
+	if err != nil {
+		return [][]string{}, err
+	}
+	defer f.Close()
+
+	// Read File into a Variable
+	lines, err := csv.NewReader(f).ReadAll()
+	if err != nil {
+		return [][]string{}, err
+	}
+
+	return lines, nil
+}
+
+func writeToFile(f *os.File, line string) {
+	_, err2 := f.WriteString(line + "\n")
+
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+}

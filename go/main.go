46c9e55d501be4d8b38682517c97e5c6d07d738f
olesmichalxp@tlen.pl <olesmichalxp@tlen.pl>
2021-06-03 19:29:42 +0200

    change rest to mux

--- go/main.go
+++ go/main.go
@@ -6,12 +6,19 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	fmt.Println("Serwer running in http://localhost:8030")
+	router := mux.NewRouter()
+	const port string = "8030"
+	router.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		fmt.Fprintln(res, "Up and running...")
+	})
 
-	http.HandleFunc("/logs", rest.GetLogs)
-	log.Fatal(http.ListenAndServe(":8030", nil))
+	router.HandleFunc("/logs", rest.GetLogs).Methods("GET")
+	// router.HandleFunc("/posts",addPosts).Methods("POST")
+	log.Printf("Serwer running in http://localhost:%s\n", port)
+	log.Fatalln(http.ListenAndServe(port, router))
 }
